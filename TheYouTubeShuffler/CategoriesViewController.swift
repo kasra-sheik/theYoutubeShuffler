52e66dedfc05b5ec52a798d61e66d2b6614dd4d9
kaasheik@gmail.com <Kasra@Kasras-MacBook-Pro.local>
2016-08-08 16:09:06 -0700

    initial commit beta

--- TheYouTubeShuffler/CategoriesViewController.swift
+++ TheYouTubeShuffler/CategoriesViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 
 class CategoriesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     @IBOutlet weak var tableView: UITableView!
-    var categoriesArray: [String] = ["Funnyy", "Sports","Science and Technology", "Music", "Animals", "Popular", "Random"]
+    var categoriesArray: [String] = ["Funny", "Sports","Science and Technology", "Music", "Animals", "Popular", "Random"]
     var imagesArray = [UIImage]()
     var selectedCategory: String = ""
     
